b2de6b839f8d3a2a3893cf15f92bb43becfe423e
Çağrı Yıldırım <cgryldrm1247@gmail.com>
2023-04-14 00:06:37 +0300

    google sign in!

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Navigation from './components/navigation/Navigation';
 
+
 export default function App() {
   return (<Navigation />
   )
